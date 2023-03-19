c3f7a76a10b3aa66ae2c26a72cf2d51321647cda
robonexx <robertwagar@gmail.com>
2023-03-19 18:49:11 +0100

    Jennys landing

---
+++ src/components/landing/Landing2.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react'
+import Image from 'next/image'
+import styles from '../landing/Landing.module.css'
+import backgroundImage from '../../../public/images/bakgrundsbild.png'
+import Cirklar from '../../../public/images/cirklar.png'
+import logo from '../../../public/images/skelleftea_logo_vit.png'
+import Link from 'next/link'
+import Button from '../buttons/Button'
+import { RiArrowRightLine } from 'react-icons/ri'
+
+
+export default function Landing2 ( { setHasVisited }) {
+
+  const VISITED = true
+  
+  const handleClick = () => {
+    localStorage.setItem('visitBefore', JSON.stringify(VISITED))
+    setHasVisited(true)
+  }
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.logo}>
+        <Image src={logo} alt="logo" width={107} />
+      </div>
+      <div className={styles.overlay}></div>
+      <Image 
+        className={styles.image} 
+        src={backgroundImage} 
+              alt="background" 
+              fill
+            priority
+      />
+      <Image 
+        className={styles.circles} 
+        src={Cirklar} 
+        alt="background" 
+        priority
+      />
+      <div className={styles.content}>
+        <p>Här finns jobben</p>
+        <h1>Välkommen hem!</h1>
+        <p>Arbetsmarknaden i Skellefteå blomstrar och behöver dig och din kompetens.</p>
+        <Link href='/' onClick={() => handleClick()}>
+          <Button variant='default' icon={<RiArrowRightLine />}>
+            Logga in för att leta jobb
+        </Button>
+        </Link>
+        <p className={styles.no_log}>Hoppa över logga in</p>
+      </div>
+    </div>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,40 +1,36 @@
-import '@/styles/globals.css'
-import styles from '@/styles/Loading.module.css'
-import Layout from '@/components/layout/Layout'
-import {useEffect, useState} from 'react'
-import Landing from '@/components/landing/Landing'
+import '@/styles/globals.css';
+import styles from '@/styles/Loading.module.css';
+import Layout from '@/components/layout/Layout';
+import { useEffect, useState } from 'react';
+/* import Landing from '@/components/landing/Landing' */
+import Landing2 from '@/components/landing/Landing2';
 
 const Loading = () => (
   <div className={styles.loading}>
     <p>Loading...</p>
   </div>
-)
-
+);
 
 export default function App({ Component, pageProps }) {
-  const [hasVisited, setHasVisited] = useState(false)
+  const [hasVisited, setHasVisited] = useState(false);
 
   useEffect(() => {
     const visited = JSON.parse(localStorage.getItem('visitBefore'));
-  console.log(visited);
-  
-  if (!visited) {
-    
-  } else {
-    setHasVisited(true)
-  }
-  }, [hasVisited])
+    console.log(visited);
+
+    if (!visited) {
+    } else {
+      setHasVisited(true);
+    }
+  }, [hasVisited]);
 
   return (
     <Layout>
-      {
-      hasVisited ? (
-          <Component {...pageProps} />
-          ) : (
-            <Landing setHasVisited={setHasVisited}/>
-          )
-        }
-        </Layout>        
-  
-  )
+      {hasVisited ? (
+        <Component {...pageProps} />
+      ) : (
+        <Landing2 setHasVisited={setHasVisited} />
+      )}
+    </Layout>
+  );
 }
